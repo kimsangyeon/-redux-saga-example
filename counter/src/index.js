bb25c860c0dda4e0860d7820d0c8a461a1dd2282
Kim Sangyeon <dho113@naver.com>
2021-02-16 12:20:25 +0900

    [FE] Counter Example

--- counter/src/index.js
+++ counter/src/index.js
@@ -1,4 +1,31 @@
+import '@babel/polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<div></div>, document.getElementById('root'));
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+
+import reducer from './reducers';
+import rootSaga from './sagas';
+
+import Counter from './components/Counter';
+
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+sagaMiddleware.run(rootSaga);
+
+const action = type => store.dispatch({ type });
+
+function render() {
+  ReactDOM.render(
+    <Counter
+      value={store.getState()}
+      onIncrement={() => action('INCREMENT')}
+      onDecrement={() => action('DECREMENT')}
+      onIncrementAsync={() => action('INCREMENT_ASYNC')}
+      onIncrementIfOdd={() => action('INCREMENT_IF_ODD')} />, document.getElementById('root')
+  );
+}
+
+render();
+store.subscribe(render);
