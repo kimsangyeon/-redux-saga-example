99dfe6871681f97be5f481c51bb9230f6a73932e
Kim Sangyeon <dho113@naver.com>
2021-02-27 21:42:43 +0900

    [FE] redux-saga shopping-cart example - [FE] redux-saga shopping-cart example

---
+++ shopping-cart/src/services/products.js
@@ -0,0 +1,5 @@
+export default [
+  { id: 1, title: 'Macbook Pro 16', price: 500.01, inventory: 2 },
+  { id: 2, title: 'Nike Air MAX', price: 10.99, inventory: 10 },
+  { id: 3, title: 'BMW X7', price: 7000.78, inventory: 5 },
+];
